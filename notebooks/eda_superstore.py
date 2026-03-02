248f1f6be69e1fd42d9bb9c5d224e3f572e9ec5a
Sagar Dhangar <sagar.dhangar.ds@gmail.com>
2026-03-02 14:27:58 +0530

    Add initial EDA code

--- notebooks/eda_superstore.py
+++ notebooks/eda_superstore.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import numpy as np
 
+# Load dataset
+df = pd.read_csv("../data/superstore.csv")
+
+print("========== FIRST 5 ROWS ==========")
+print(df.head())
+
+print("\n========== DATASET SHAPE ==========")
+print(df.shape)
+
+print("\n========== DATASET INFO ==========")
+print(df.info())
